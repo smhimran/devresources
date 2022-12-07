e4e9ce387e680c15218b73e66ef5da3689953fbe
Shah Habibul Imran <shah.imran@enosisbd.com>
2022-12-08 00:20:10 +0600

    BE-004: Add security config

---
+++ src/main/java/com/devreources/devresources/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.devreources.devresources.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalAuthentication
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+}
--- src/main/java/com/devreources/devresources/controllers/CategoryController.java
+++ src/main/java/com/devreources/devresources/controllers/CategoryController.java
@@ -4,17 +4,19 @@ import com.devreources.devresources.models.Category;
 import com.devreources.devresources.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController()
+@RestController
+@RequestMapping("/api/v1/category")
 public class CategoryController {
 
     @Autowired
     CategoryRepository repository;
 
-    @GetMapping("/categories")
+    @GetMapping("/")
     public List<Category> getAllCategories() {
         return repository.findAll();
     }
